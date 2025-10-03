7a7d7705f282b9989a40f2569915af38b0a6962c
CC230286 <efraincortez777@gmail.com>
2025-10-02 20:20:04 -0600

    Contra de la db
    solo para hacer las pruebas en mi db pero si las suyas no tienen contra lo dejan en blanco

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: '2005',
     database: 'tiendaya',
     waitForConnections: true,
     connectionLimit: 10,
